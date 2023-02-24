d2cfddce2feaac667ea47dae2c8d9c1021250fde
Michael Tapia <mtapia19@students.mchenry.edu>
2023-02-23 19:32:41 -0600

    MT Operator Exercise

---
+++ MTCalculator.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+let greeting = "Hello, playground"
+
+// Top section
+let a = 20;
+let b = 19;
+let c = 72;
+let d = 24;
+let e = 12;
+let f = 3;
+
+// Middle Section
+let plus = (a + b);
+let minus = (c - d);
+let mult = (e * f);
+let div = (d / e);
+let percent = (c % b)
+
+// Bottom Section
+print("Plus: ", plus, "\nMinus ", minus, "\nMultiplication: ", mult, "\nDivision: ", div, "\nModulus: ", percent)
